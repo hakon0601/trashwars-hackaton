6573801827f518d89822e317bfba7f20875b2731
Håkon Gimse <hakongimse@gmail.com>
2019-03-22 12:51:57 +0100

    secrets...

---
+++ public/secrets.js
@@ -0,0 +1,9 @@
+# 1 "ok/trashwars-hackaton/public/secrets.js"
+# 1 "<built-in>" 1
+# 1 "<built-in>" 3
+# 360 "<built-in>" 3
+# 1 "<command line>" 1
+# 1 "<built-in>" 2
+# 1 "ok/trashwars-hackaton/public/secrets.js" 2
+window.GOOGLE_MAPS_API_KEY = 'AIzaSyBKBZgE2rh6lC9Sp5GuUMFM9OuAEUDYIkU';
+
